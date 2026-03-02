15fb618f09ff8bdd039c4313fe1ded13cc07e4a9
Zack <396514268@qq.com>
2026-03-02 16:15:57 +0800

    修复查看大图问题

---
+++ YBImageBrowser/YBMarcos.h
@@ -0,0 +1,17 @@
+//
+//  YBMarcos.h
+//  YBImageBrowserDemo
+//
+//  Created by MacStudio on 2026/3/2.
+//  Copyright © 2026 杨波. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YBMarcos : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YBImageBrowser/YBMarcos.m
@@ -0,0 +1,13 @@
+//
+//  YBMarcos.m
+//  YBImageBrowserDemo
+//
+//  Created by MacStudio on 2026/3/2.
+//  Copyright © 2026 杨波. All rights reserved.
+//
+
+#import "YBMarcos.h"
+
+@implementation YBMarcos
+
+@end
